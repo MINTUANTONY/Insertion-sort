d47c463e77925f221cf3be53ce20216832bfd529
MINTU ANTONY <47614@user>
2017-06-23 10:53:47 -0400

    Insertion sort

--- insertion.c
+++ insertion.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
- 
-int main()
+int c,d,t;
+int sort(int *array)
 {
-  int n, array[7]={4,23,43,38,15,10,30}, c, d, t;
- 
- 
-  for (c = 1 ; c <= n - 1; c++) {
+for (c = 1 ; c <= 7 - 1; c++) {
     d = c;
  
     while ( d > 0 && array[d] < array[d-1]) {
@@ -16,12 +13,16 @@ int main()
       d--;
     }
   }
+}
+int main()
+{
+  int n, array[7]={4,23,43,38,15,10,30};
  
+  sort(array);
   printf("Sorted list in ascending order:\n");
  
-  for (c = 0; c <= n - 1; c++) {
+  for (c = 0; c <= 7- 1; c++) {
     printf("%d\n", array[c]);
   }
  
-  return 0;
 }
